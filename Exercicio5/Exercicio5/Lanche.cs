a16e4c153490103d8b74ee8b86031fe9825bf1d8
Gabriel Destro <gabrielaugustodestro@gmail.com>
2022-06-16 14:33:34 -0300

    Solucao Ex 5

---
+++ Exercicio5/Exercicio5/Lanche.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    public class Lanche : LanchePrototype
+    {
+        private string Pao { get; set; }
+        private string Recheio { get; set; }
+        private string Condimentos { get; set; }
+
+        public Lanche(string _Pao, string _Recheio, string _Condimentos)
+        {
+            Pao = _Pao;
+            Recheio = _Recheio;
+            Condimentos = _Condimentos;
+        }
+
+        public void Ingredientes()
+        {
+            Console.WriteLine("Exibindo ingredientes do lanche:");
+            Console.WriteLine($"{Pao}\n{Recheio}\n{Condimentos}\n");
+        }
+
+        public override LanchePrototype Clone()
+        {
+            return (LanchePrototype)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercicio5;
+
+Console.WriteLine("Clonando lanches!\n");
+
+Lanche cachorroQuente = new Lanche("Pão de Cachorro Quente", "Salsicha", "Mostarda");
+Lanche cachorroQuenteClone = (Lanche)cachorroQuente.Clone();
+
+cachorroQuente.Ingredientes();
+cachorroQuenteClone.Ingredientes();
