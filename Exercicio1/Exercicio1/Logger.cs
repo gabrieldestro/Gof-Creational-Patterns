11df6bee9f8c64aca02b1b73a9c28fca102bf051
Gabriel Destro <gabrielaugustodestro@gmail.com>
2022-06-16 14:32:16 -0300

    Solucao ex 1

---
+++ Exercicio1/Exercicio1/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    public sealed class Logger
+    {
+        private static readonly Logger _Logger = new Logger();
+        private Logger()
+        {}
+        public static Logger GetLogger
+        {
+            get 
+            {
+                return _Logger;
+            }
+        }
+        public void Registrar(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
